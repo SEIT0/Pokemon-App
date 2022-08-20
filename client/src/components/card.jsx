ef7c863a6a36ef1ce7a96145fc00121e8e1268b4
SEIT0 <seito99@gmail.com>
2022-08-20 12:09:49 -0300

    fix card img size

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import decamark from '../media/Spr_3r_000.png'
 
 function Card(p) {
+
+  const PokeImg = styled.img`
+    object-fit: cover;
+    width: 96px;
+    height: 96px;
+  `
 
   let {front_default, name, stats, types, Types, className} = p
 
   return (
     <div className={className}>
       <Link to={'/detail/'+name}>{name.charAt(0).toUpperCase() + name.slice(1)}</Link> <br />
-      <img src={front_default} alt='' onError={e=>{if(e.target.src!==decamark){e.target.onerror = null; e.target.src=decamark}}}/>
+      <PokeImg src={front_default} alt='' onError={e=>{if(e.target.src!==decamark){e.target.onerror = null; e.target.src=decamark}}}/>
       {Types? <h3>Types {Types[0]?.name} {Types[1]?.name}</h3> : <h3>Types {types[0]?.type.name} {types[1]?.type.name}</h3>}
       <h4>atk: {stats[1]?.base_stat}</h4>
     </div>
